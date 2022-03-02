6f3216bf7767b82834dadf01d65459f199afe572
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2022-03-02 14:48:16 +0100

    Update trafic.php
    correction de la liste ordonnée et mise en place de paragraphes

--- trafic.php
+++ trafic.php
@@ -1,6 +1,6 @@
 
-            <h3>Qu'est-ce que le braconnage ?</h3>
-<h5>Le braconnage est un acte de chasse ou de pêche illégal. Son illégalité s’exprime suivant différentes conditions :</h5>
+            <h2>Qu'est-ce que le braconnage ?</h2>
+<p>Le braconnage est un acte de chasse ou de pêche illégal. Son illégalité s’exprime suivant différentes conditions :</p>
 <ul>
     <li>Il peut s’agir d’un acte d’abattage d’espèces intégralement protégées</li>
     <li>Ou il se réalise dans une zone intégralement protégée, quel que soit le statut de l’espèce abattue</li>
@@ -8,17 +8,31 @@
     <li>Il a lieu à une époque où la chasse est interdite</li>
     <li>Sans autorisation (pas de quota pour l’espèce, absence de permis ou de timbre, …)</li>
 </ul>
-<h3>Quels sont les différents types de braconnage ?</h3> 
+<h2>Quels sont les différents types de braconnage ?</h2> 
 <img src="arme-braconnage.jpg" class="centered">
-<ol><h4>1. Le Braconnage coutumier</h4></ol>
-<h5>Il s’agit d’un braconnage relevant de traditions ancestrales. </h5>
-<ol><h4>2. Le Braconnage de subsistance</h4> </ol>
-<h5>Devant un manque cruel de nourriture protéinée, les villageois ont souvent tendance à piéger des animaux pour améliorer l’ordinaire.</h5>
-<ol><h4>3. Le Braconnage de représailles</h4></ol>
-<h5>les surfaces agricoles et l’élevage rentrent en concurrence directe avec la faune sauvage à proximité. D’un côté les prédateurs préfèrent attaquer le bétail sans défense, les éléphants se nourrir aisément dans les champs, et de l’autre les agriculteurs et éleveurs voient leur maigre patrimoine ponctionner par les animaux sauvages. L’opposition qui en résulte alors se caractérise par des actions délibérées des populations humaines pour se venger des agressions des animaux. Il peut s’agir de tirs directs, de pièges, d’empoisonnement.</h5>
-<img src="deforestation.jpg" class="centered">
-<ol><h4>4. Le Braconnage commercial</h4></ol>
-<h5>Des structures informelles commercialisent la « viande de brousse » sur les bords de route et approvisionnent restaurants et industries alimentaires.</h5>
-<ol><h4>5. Le Braconnage mafieux</h4></ol>
-<h5>Ce type de braconnage s’appuie en fait sur des entités internationales de type mafieux qui trafiquent les espèces animales en créant de nouveaux courants économiques (ivoire, cornes, chair, …).</h5>
+<ol> 
+    <li>Le Braconnage coutumier
+    <p>Il s’agit d’un braconnage relevant de traditions ancestrales. </p>
+    </li>
+    <li>Le Braconnage de subsistance
+        <p>Devant un manque cruel de nourriture protéinée, les villageois ont souvent tendance à piéger des
+             animaux pour améliorer l’ordinaire.</p>
+    </li>
+    <li>Le Braconnage de représailles
+    <p>les surfaces agricoles et l’élevage rentrent en concurrence directe avec la faune sauvage à proximité.
+         D’un côté les prédateurs préfèrent attaquer le bétail sans défense, les éléphants se nourrir 
+         aisément dans les champs, et de l’autre les agriculteurs et éleveurs voient leur maigre patrimoine 
+         ponctionner par les animaux sauvages. L’opposition qui en résulte alors se caractérise par des 
+         actions délibérées des populations humaines pour se venger des agressions des animaux. Il peut 
+         s’agir de tirs directs, de pièges, d’empoisonnement.</p>
+         <img src="deforestation.jpg" class="centered">
+    </li>
+    <li>Le Braconnage commercial
+<p>Des structures informelles commercialisent la « viande de brousse » sur les bords de route et
+     approvisionnent restaurants et industries alimentaires.</p>
+</li>
+<li>Le Braconnage mafieux
+<p>Ce type de braconnage s’appuie en fait sur des entités internationales de type mafieux qui trafiquent les espèces animales en créant de nouveaux courants économiques (ivoire, cornes, chair, …).></p>
+</li>
+</ol>
 <h2 class="text_centered"> Sauvons les animaux !</h2>
