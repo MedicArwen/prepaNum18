3c92b1066c4b69e057dd25efd6c562527a07ec8c
Nezerkk <aznhqk@gmail.com>
2022-03-02 14:04:33 +0100

    Update index.php
    p

--- index.php
+++ index.php
@@ -20,7 +20,11 @@ include('header.php')
     <div id="general">
         <?php include('nav.php');?>
         <div id="main">
+<<<<<<< Updated upstream
     
+=======
+            yolo les potos
+>>>>>>> Stashed changes
         <?php 
         if (!isset($_GET['page']))
         include('accueil.php');
