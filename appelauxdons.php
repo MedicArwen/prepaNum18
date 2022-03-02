9c34919420882ba7ed85ca65673f5bcac89daadb
Sarah <sarah.bedreddine.via@gmail.com>
2022-03-02 14:01:04 +0100

    Update appelauxdons.php

--- appelauxdons.php
+++ appelauxdons.php
@@ -1,6 +1,6 @@
 
             <p>Tous les jours, des annimaux meurent de braconnage. Si vous souhaitez nous aider à combattre cette injustice, vous pouvez nous faire un don qui sera reversé aux organisations anti-braconniers.</p>
-            <p>Par exemple 6€, soit le prix d'un kebab.</p>
+            <p>Par exemple 6€, soit le prix d'un kebab. Ils vous remercient !</p>
             <div class="reference">
             <p><a href="paypal">Paypal</a></p>
             <p><a href="rib">Rib</a></p>
